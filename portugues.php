b5567cbc1ca29320974cd7b617cedfa575d6c533
Geovana Eduarda <geovana.e.oliveira@aluno.senai.br>
2026-03-13 09:29:54 -0300

    atualizações

--- portugues.php
+++ portugues.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dicionário de Português - SENAI</title>
-    <link rel="stylesheet" href="assets/css/style.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
